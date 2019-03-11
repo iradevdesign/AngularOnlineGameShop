0bd4f9d1446803e9a40874deec62df70d92a73fe
robert-dev <hacked666>
2019-03-11 18:26:29 +0200

    second

---
+++ src/private/firebase-key.ts
@@ -0,0 +1,8 @@
+export const fireBaseKey = {
+    apiKey: "AIzaSyDGW3O3bTklgDi6Eol4wVDDyIwyaozrKEM",
+    authDomain: "onlinegameshop-1603f.firebaseapp.com",
+    databaseURL: "https://onlinegameshop-1603f.firebaseio.com",
+    projectId: "onlinegameshop-1603f",
+    storageBucket: "onlinegameshop-1603f.appspot.com",
+    messagingSenderId: "1048870133214"
+};
